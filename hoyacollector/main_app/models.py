26995efb253011649167512c6952ee7cc26f91c2
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-07-20 17:01:36 -0400

    add cultivation model

--- hoyacollector/main_app/models.py
+++ hoyacollector/main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples
+CARES = (
+    ('W', 'Watering'),
+    ('S', 'Sun light'),
+    ('N', 'Nutrition')
+)
+
 # Create your models here.
 class Hoya(models.Model):
     name = models.CharField(max_length=100)
@@ -14,3 +21,15 @@ class Hoya(models.Model):
       # Add this method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'hoya_id': self.id})
+
+class cultivation(models.Model):
+    date = models.DateField("Caring Date")
+    care = models.CharField(max_length=1,
+    choices=CARES, 
+    default=CARES[0][0])
+    # create a hoya_id FK
+    hoya = models.ForeignKey(Hoya, on_delete=models.CASCADE)
+
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+      return f"{self.get_care_display()} on {self.date}"
